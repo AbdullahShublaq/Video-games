636d0c7570fdcbdbfcea7ea5ccfda26e5499605a
Abdullah Shublaq <abd.shublaq@gmail.com>
2021-01-29 21:07:06 +0200

    refactor to blade components

--- resources/views/livewire/most-anticipated.blade.php
+++ resources/views/livewire/most-anticipated.blade.php
@@ -1,34 +1,12 @@
 <div wire:init="loadMostAnticipated">
     @forelse($mostAnticipated as $game)
         <div class="most-anticipated-container space-y-10 mt-8">
-            <div class="game flex">
-                <a href="{{ route('games.show', $game['slug']) }}">
-                    <img src="{{ $game['coverImageUrl'] }}" class="w-16 hover:opacity-75 transition ease-in-out duration-150" alt="Cover">
-                </a>
-                <div class="ml-4">
-                    <a href="{{ route('games.show', $game['slug']) }}" class="hover:text-gray-300">
-                        {{ $game['name'] }}
-                    </a>
-                    <div class="text-gray-400 text-sm mt-1">
-                        {{ $game['first_release_date'] }}
-                    </div>
-                </div>
-            </div>
+            <x-game-card-small :game="$game"/>
         </div>
     @empty
         @foreach(range(1, 4) as $game)
             <div class="most-anticipated-container space-y-10 mt-8">
-                <div class="game flex">
-                    <div class="bg-gray-800 w-16 h-20 flex-none"></div>
-                    <div class="ml-4">
-                        <div class="text-transparent bg-gray-700 rounded leading-tight">
-                            Title goes here today
-                        </div>
-                        <div class="text-transparent bg-gray-700 rounded inline-block text-sm mt-2">
-                            Jan 25, 2020
-                        </div>
-                    </div>
-                </div>
+                <x-game-card-small-skeleton/>
             </div>
         @endforeach
     @endforelse
