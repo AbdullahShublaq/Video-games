774d43acfd784c6b30e9450a83ff74f957519a54
Abdullah Shublaq <abd.shublaq@gmail.com>
2021-02-01 14:00:47 +0200

    search dropdown UX with Alpinejs

--- resources/views/livewire/search-dropdown.blade.php
+++ resources/views/livewire/search-dropdown.blade.php
@@ -1,8 +1,20 @@
-<div class="relative">
-    <input wire:model.debounce.300ms="search"
+<div class="relative" x-data="{ isVisible: true }" @click.away="isVisible = false">
+    <input
+           wire:model.debounce.300ms="search"
+           @focus="isVisible = true"
+           @keydown.escape.window = "isVisible = false"
+           @keydown = "isVisible = true"
+           @keydown.shift.tab = "isVisible = false"
+           x-ref="search"
+           @keydown.window="
+                if(event.keyCode == 191) {
+                    event.preventDefault();
+                    $refs.search.focus();
+                }
+            "
            type="text"
            class="bg-gray-800 text-sm rounded-full w-64 px-3 py-1 pl-8 focus:outline-none focus:shadow-outline"
-           placeholder="Search...">
+           placeholder="Search (Press '/' to focus)">
     <div class="absolute top-0 flex items-center h-full ml-2">
         <svg class="fill-current text-gray-400 w-4" viewBox="0 0 24 24">
             <path class="heroicon-ui"
@@ -13,12 +25,13 @@
     <div wire:loading class="spinner top-0 right-0 mr-4 mt-3" style="position: absolute"></div>
 
     @if(strlen($search) >= 1)
-        <div class="absolute z-50 bg-gray-800 text-xs rounded w-64 mt-2">
+        <div class="absolute z-50 bg-gray-800 text-xs rounded w-64 mt-2" x-show.transition.opacity.duration.200="isVisible">
             @if(count($searchResults) > 0)
                 <ul>
                     @foreach($searchResults as $game)
                         <li class="border-b border-gray-700">
-                            <a href="{{ route('games.show', $game['slug']) }}" class="block hover:bg-gray-700 px-3 py-3 flex items-center transition ease-in-out duration-150">
+                            <a @if($loop->last) @keydown.tab = "isVisible = false" @endif
+                                href="{{ route('games.show', $game['slug']) }}" class="block hover:bg-gray-700 px-3 py-3 flex items-center transition ease-in-out duration-150">
                                 @if(isset($game['cover']))
                                     <img src="{{ Str::replaceFirst('thumb', 'cover_small', $game['cover']['url']) }}" class="w-10" alt="cover">
                                 @else
